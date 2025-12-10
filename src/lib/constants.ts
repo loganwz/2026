244fae0c5eb5312e69ef99c618a44c22a2cd9a7f
loganwz <marliziegler24@gmail.com>
2025-12-09 23:34:19 -0300

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -297,4 +297,4 @@ export const MATERIALS: Material[] = [
   }
 ];
 
-export const KIWIFY_CHECKOUT_URL = 'https://pay.kiwify.com.br/SEU_LINK_AQUI';
+export const KIWIFY_CHECKOUT_URL = https://pay.kiwify.com.br/SoriU9c
